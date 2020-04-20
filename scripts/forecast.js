3e489200295675ed534813372f91f66e2199366e
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-04-20 19:15:36 +0300

    add api call for city

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,16 @@
 const key = '33PF7HJhi4ky6ArNLbG4LAJFNSf28CGC';
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+}
+
+getCity('helsinki')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
